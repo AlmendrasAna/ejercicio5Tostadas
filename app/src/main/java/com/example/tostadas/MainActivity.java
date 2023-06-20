84d63f5f96a73f5854c51fc5e633a0b834a466c8
Ana <almendras451@gmail.com>
2023-06-19 21:02:16 -0400

    ShowToast listo

--- app/src/main/java/com/example/tostadas/MainActivity.java
+++ app/src/main/java/com/example/tostadas/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.tostadas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initListener();
 
+
+
+    }
+
+    private void initListener() {
+
+        Button toastB = findViewById(R.id.buttonToast);
+        Button clickB = findViewById(R.id.buttonCount);
+        TextView showCount = findViewById(R.id.showCount);
+
+        toastB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Hello Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        clickB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer contador = Integer.parseInt(showCount.getText().toString()) + 1;
+                showCount.setText(contador);
+            }
+        });
     }
 }
